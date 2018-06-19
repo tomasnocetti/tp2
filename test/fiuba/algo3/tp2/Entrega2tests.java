c58088d0e1821cf129f7c267a44d4f019a563540
Tomas Nocetti <tomas@gm2dev.com>
2018-06-19 16:20:10 -0300

    Entrega2

---
+++ test/fiuba/algo3/tp2/Entrega2tests.java
@@ -0,0 +1,64 @@
+package fiuba.algo3.tp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Entrega2tests {
+	
+	@Test
+	public void test01colocarCartaMagicaWastelandEnCampoYVerificarEfecto() {
+		
+	}
+	
+	@Test
+	public void test02colocarCartaMagicaSogenEnCampoYVerificarEfecto() {
+		
+	}
+
+	@Test
+	public void test03activarCartaMagicaOllaDeLaCodiciaYVerificarEfecto() {
+		
+	}
+	
+	@Test
+	public void test04activarCartaMagicaFisuraYVerificarEfecto() {
+		
+	}
+
+	@Test
+	public void test05colocarJinzo7EnElCampoVerificarAtaqueALosPuntosDeVidaDirecto() {
+		
+	}
+
+	@Test
+	public void test06invocarAlDragonDefinitivoDeOjosAzulesSacrificando3DragonesBlancos() {
+		
+	}
+	
+	@Test
+	public void test07colocarInsectoComeHombresEnDefensaBocaAbajoYProbarEfectoEnBatalla() {
+		
+	}
+
+	@Test
+	public void test08colocarCartaTrampaCilindroMagicoDelLadoDelCampoYAtacarConElMonstruoEnemigoVerificandoEfecto() {
+		
+	}
+
+	@Test
+	public void test09colocarCartaTrampaReinforcementsYProbarEfectoEnBatalla() {
+		
+	}
+
+	@Test
+	public void test10extraerTodasLasCartasDelMazoYVerificarPartidaTerminada() {
+		
+	}
+
+	@Test
+	public void test11colocarPartesDeExodiaEnLaManoYVerificarPartidaTerminada() {
+		
+	}
+
+}
