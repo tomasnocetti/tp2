e3f2c52fb45e7dba5ccea5acdd2edae8c5531628
Tomas Nocetti <tomas@gm2dev.com>
2018-06-20 17:48:11 -0300

    Librerias faltantes

--- src/fiuba/algo3/Efectos/EfectoAgujeroNegro.java
+++ src/fiuba/algo3/Efectos/EfectoAgujeroNegro.java
@@ -1,6 +1,9 @@
 	package fiuba.algo3.Efectos;
 
 import fiuba.algo3.tp2.Jugador;
+import fiuba.algo3.tp2.Cartas.Carta;
+import fiuba.algo3.tp2.Cartas.CartaMonstruo;
+import fiuba.algo3.tp2.Tableros.ZonaMano;
 
 public class EfectoAgujeroNegro extends Efecto {
 	
