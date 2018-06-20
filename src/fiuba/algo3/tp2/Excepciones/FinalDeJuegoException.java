0c087de56f4b0ebd8f6360e8b4e44f4660bf8358
jowi <joelsaidman1@gmail.com>
2018-06-20 19:45:02 -0300

    saco atributo cantidad de cartas de mazo y agrego excepcion de final de juego

---
+++ src/fiuba/algo3/tp2/Excepciones/FinalDeJuegoException.java
@@ -0,0 +1,7 @@
+package fiuba.algo3.tp2.Excepciones;
+
+public class FinalDeJuegoException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+}
--- src/fiuba/algo3/tp2/Tableros/Mazo.java
+++ src/fiuba/algo3/tp2/Tableros/Mazo.java
@@ -8,28 +8,25 @@ import fiuba.algo3.tp2.Cartas.Carta;
 
 public class Mazo{
 	
-	private int cantidadDeCartas;
 	private ArrayList<Carta> cartas;
 	private Jugador jugador;
 	
 	public Mazo(Jugador _jugador, ArrayList<Carta> _cartas) {
-		this.cantidadDeCartas = 40;
 		this.cartas = _cartas;
 		this.jugador = _jugador;
 	}
 
 	public Carta agarrarCarta() {
-		if (this.cantidadDeCartas == 0) {
+		if (this.cartas.size() == 0) {
 			Juego.ObtenerJuego().asignarPerdedor(this.jugador);
 			return null;
 		}
-		Carta carta = cartas.remove(cantidadDeCartas - 1);
-		this.cantidadDeCartas -=1;
+		Carta carta = cartas.remove(this.cartas.size() - 1);
 		return carta;
 	}
 
 	public int obtenerCantidadDeCartas() {
-		return this.cantidadDeCartas;
+		return this.cartas.size();
 	}
 	
 	
