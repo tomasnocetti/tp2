4284327621f0b8cd0856d734cf903f2dc35fa7d9
jowi <joelsaidman1@gmail.com>
2018-06-21 14:09:36 -0300

    agrego observer a vista

--- src/vista/JuegoObserver.java
+++ src/vista/JuegoObserver.java
@@ -1,21 +1,22 @@
-package fiuba.algo3.tp2;
+package vista;
 
 import java.awt.Frame;
 import java.io.PrintStream;
 import java.util.Observable;
 import java.util.Observer;
 
-public class JuegoObserver extends Frame implements Observer {
-	private static final long serialVersionUID = 1L;
-	private Juego juego;
+import fiuba.algo3.tp2.Juego;
 
-    public JuegoObserver( Juego juego ) {
-    	this.juego = juego;
-    }
+public class JuegoObserver extends Frame implements Observer {
+	
+	private static final long serialVersionUID = 1L;
 
     public void update( Observable obs,Object obj ) {
-        if( obs != juego) return;
+        if( obs != Juego.ObtenerJuego()) return;
         PrintStream out = System.out;
-        out.println( juego.estadoDelJuegoTerminado() );
+        out.println( Juego.ObtenerJuego().estadoDelJuegoTerminado() );
     }
 }
+
+
+//instancia de la vista (scene) para poder ejecutar el alert (juego va ser singleton y lo podemos acceder directamente)
