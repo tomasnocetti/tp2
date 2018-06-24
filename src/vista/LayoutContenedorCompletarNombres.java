fe6e9c41d91340ca74dac6b2426020748e183717
jowi <joelsaidman1@gmail.com>
2018-06-23 22:18:36 -0300

    refactorizacion de fondo nombre

--- src/vista/LayoutContenedorCompletarNombres.java
+++ src/vista/LayoutContenedorCompletarNombres.java
@@ -6,9 +6,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import vista.eventHandlers.BotonJugarEventHandler;
 
@@ -19,31 +26,59 @@ public class LayoutContenedorCompletarNombres extends VBox{
 		this.getStylesheets().addAll(AlGoHo.class.getResource("style.css").toExternalForm());
 		this.getStyleClass().add("fondoNombres");
 		
-		GridPane grid = new GridPane();
-		grid.setPadding(new Insets(10, 10, 10, 10));
-		grid.setVgap(10);
-		grid.setHgap(90);
 		
-		TextField name = new TextField();
-	    name.setPromptText("Enter name player one");
-	    name.setPrefColumnCount(10);
-	    name.setMaxWidth(300);
-	    GridPane.setConstraints(name, 0, 0);
-	    grid.getChildren().add(name);
+		
+//		GridPane grid = new GridPane();
+//		grid.setPadding(new Insets(10, 10, 10, 10));
+//		grid.setVgap(10);
+//		grid.setHgap(90);
+		
+		VBox vbox1 = new VBox(20);
+		VBox vbox2 = new VBox();
+		VBox vbox3 = new VBox();
+		
+		vbox1.setPrefSize(300,300);
+		vbox2.setPrefSize(300,300);
+		vbox3.setPrefSize(500,300);
+		
+		Text titulo = new Text(300, 300, "Bienvenidos a YuGiOh");
+		titulo.setFill(Color.DARKVIOLET);
+		titulo.setFont(Font.font(java.awt.Font.SERIF, FontWeight.EXTRA_BOLD, 60));
+	    final Light.Distant light = new Light.Distant();
+	    light.setAzimuth(-135.0);
+	    final Lighting lighting = new Lighting();
+	    lighting.setLight(light);
+	    lighting.setSurfaceScale(9.0);
+		vbox3.getChildren().add(titulo);
+		
+		TextField name1 = new TextField();
+	    name1.setPromptText("Enter name player one");
+	    name1.setPrefColumnCount(10);
+	    name1.setMaxWidth(300);
+	    GridPane.setConstraints(name1, 0, 0);
+	    vbox1.getChildren().add(name1);
+	    
+		TextField name2 = new TextField();
+	    name2.setPromptText("Enter name player two");
+	    name2.setPrefColumnCount(10);
+	    name2.setMaxWidth(300);
+	    GridPane.setConstraints(name2, 0, 0);
+	    vbox1.getChildren().add(name2);
 	    
 	    Button buttonJugar = new Button("Jugar");
 	    GridPane.setConstraints(buttonJugar, 1, 0);
-	    grid.getChildren().add(buttonJugar);
+	    vbox2.getChildren().add(buttonJugar);
 	    buttonJugar.setOnAction(new BotonJugarEventHandler(stage, sceneJuego, mediaPlayer));
 	    
+	    //Label label = new Label();
+	    //this.getChildren().addAll(label, name);
+	    vbox1.setAlignment(Pos.CENTER);
+	    vbox2.setAlignment(Pos.CENTER);
+	    vbox3.setAlignment(Pos.CENTER);
 	    
-	    Button clear = new Button("Borrar");
-	    GridPane.setConstraints(clear, 1, 1);
-	    grid.getChildren().add(clear);
-	    
-	    Label label = new Label();
-	    this.getChildren().addAll(label, name);
-		this.getChildren().add(grid);
+	    this.getChildren().add(vbox3);
+	    this.getChildren().add(vbox1);
+		this.getChildren().add(vbox2);
 		
 		this.setAlignment(Pos.CENTER);
 	    this.setSpacing(15);
