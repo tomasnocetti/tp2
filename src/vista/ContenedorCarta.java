87690d3073052d454e90984ffbee798bfda40e3a
JuliMej <jmejliker@gmail.com>
2018-06-26 22:52:27 -0300

    Grafica posicion psicion y accion implementadas

--- src/vista/ContenedorCarta.java
+++ src/vista/ContenedorCarta.java
@@ -1,8 +1,14 @@
 package vista;
 
+import fiuba.algo3.Estados.AccionDefensa;
+import fiuba.algo3.Estados.MonstruoPosicionAbajo;
+import fiuba.algo3.Estados.PosicionAbajo;
 import fiuba.algo3.tp2.Cartas.Carta;
 import fiuba.algo3.tp2.Cartas.CartaMonstruo;
 import javafx.geometry.Insets;
+import javafx.geometry.NodeOrientation;
+import javafx.geometry.Orientation;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -22,31 +28,55 @@ public class ContenedorCarta extends VBox {
 		this.setMinHeight(180);
 		this.setMinWidth(150);
 		this.carta = carta;
-		if(carta instanceof CartaMonstruo) {
-			Text nombre = new Text(carta.nombre());
-			nombre.setWrappingWidth(100);
-			Text ataque = new Text("ATK: " + ((CartaMonstruo) carta).obtenerPuntosAtaque());
-			Text defensa = new Text("DEN: " + ((CartaMonstruo) carta).obtenerPuntosDefensa());
-			Text estrellas = new Text("EST: " + ((CartaMonstruo) carta).obtenerEstrellas());
-			Text tipo = new Text(carta.getClass().getSimpleName());
-			this.getChildren().addAll(nombre,tipo,ataque,defensa, estrellas);
-			this.setMargin(this.getChildren().get(0),new Insets(5));
-	        this.setMargin(this.getChildren().get(1),new Insets(5));
-	        this.setMargin(this.getChildren().get(2),new Insets(5));
-	        this.setMargin(this.getChildren().get(3),new Insets(5));
-	        this.setMargin(this.getChildren().get(4),new Insets(5));
-		}else {
-			Text nombre = new Text(carta.nombre());
-			nombre.setWrappingWidth(150);
-			Text tipo = new Text(carta.getClass().getSimpleName());
-			this.getChildren().addAll(nombre,tipo);
-			this.setMargin(this.getChildren().get(0),new Insets(5));
-			this.setMargin(this.getChildren().get(1),new Insets(5));
-		}
-		
+		this.dibujar();
 	}
 
 	public Carta obtenerCarta() {
 		return carta;
 	}
+	
+	public void dibujar() {
+		this.getChildren().clear();
+		if(carta.obtenerPosicion() instanceof PosicionAbajo || carta.obtenerPosicion() instanceof MonstruoPosicionAbajo) {
+			VBox box = new VBox();
+			Button button = new Button();
+
+			button.getStylesheets().addAll(AlGoHo.class.getResource("style.css").toExternalForm());
+			button.getStyleClass().add("button-cartas");
+			button.setPrefHeight(250);
+			button.setPrefWidth(200);
+			box.getChildren().add(button);
+			this.getChildren().add(box);
+			System.out.println("Abajo");
+			System.out.println(this.getChildren());
+		}else {
+			if(carta instanceof CartaMonstruo) {
+				VBox box = new VBox();
+				Text nombre = new Text(carta.nombre());
+				nombre.setWrappingWidth(100);
+				Text ataque = new Text("ATK: " + ((CartaMonstruo) carta).obtenerPuntosAtaque());
+				Text defensa = new Text("DEN: " + ((CartaMonstruo) carta).obtenerPuntosDefensa());
+				Text estrellas = new Text("EST: " + ((CartaMonstruo) carta).obtenerEstrellas());
+				Text tipo = new Text(carta.getClass().getSimpleName());
+				this.getChildren().addAll(nombre,tipo,ataque,defensa, estrellas);
+				box.setMargin(this.getChildren().get(0),new Insets(5));
+		        box.setMargin(this.getChildren().get(1),new Insets(5));
+		        box.setMargin(this.getChildren().get(2),new Insets(5));
+		        box.setMargin(this.getChildren().get(3),new Insets(5));
+		        box.setMargin(this.getChildren().get(4),new Insets(5));
+		        CartaMonstruo cartaMon = (CartaMonstruo) carta;
+		        if(cartaMon.obtenerAccion() instanceof AccionDefensa) {
+		        	this.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+		        }
+			}else {
+				Text nombre = new Text(carta.nombre());
+				nombre.setWrappingWidth(150);
+				Text tipo = new Text(carta.getClass().getSimpleName());
+				this.getChildren().addAll(nombre,tipo);
+				this.setMargin(this.getChildren().get(0),new Insets(5));
+				this.setMargin(this.getChildren().get(1),new Insets(5));
+			}
+		}	
+	}
+
 }
--- src/vista/ContenedorCartaMazo.java
+++ src/vista/ContenedorCartaMazo.java
@@ -9,18 +9,16 @@ public class ContenedorCartaMazo extends VBox {
 	
 	public ContenedorCartaMazo(String cartas, int num_jugador, VistaMazo vista) {
 		Button button = new Button(cartas);
-		this.getChildren().add(button);
-		this.setAlignment(Pos.CENTER);
-		this.setPrefHeight(180);
-		this.setPrefWidth(150);
-		
-//		ButtonMazoEventHandler buttonMazoEventHandler = new ButtonMazoEventHandler(num_jugador, vista,0,button); 
-//		button.setOnAction(buttonMazoEventHandler);
 
-		
 		button.getStylesheets().addAll(AlGoHo.class.getResource("style.css").toExternalForm());
 		button.getStyleClass().add("button-cartas");
 		button.setPrefHeight(250);
 		button.setPrefWidth(200);
+		
+		this.getChildren().add(button);
+		this.setAlignment(Pos.CENTER);
+		this.setPrefHeight(180);
+		this.setPrefWidth(150);
+
 	}
 }
